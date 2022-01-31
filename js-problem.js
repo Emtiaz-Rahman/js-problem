28589b712fa134d48d6cecaf3bc6403e72d2cbaf
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-01-31 23:57:35 +0600

    problem round

--- js-problem.js
+++ js-problem.js
@@ -1,12 +1,16 @@
-const myNumber = -5;
-const outPut = Math.abs(myNumber);
-console.log(outPut);
+// const myNumber = -5;
+// const outPut = Math.abs(myNumber);
+// console.log(outPut);
 
 
-const myNumber = 7.0322;
-const outPut = Math.ceil(myNumber);
-console.log(outPut);
+// const myNumber = 7.0322;
+// const outPut = Math.ceil(myNumber);
+// console.log(outPut);
 
-const myNumber = 6.003;
-const outPut = Math.floor(myNumber);
+// const myNumber = 6.003;
+// const outPut = Math.floor(myNumber);
+// console.log(outPut);
+
+const myNumber = 6.403;
+const outPut = Math.round(myNumber);
 console.log(outPut);
