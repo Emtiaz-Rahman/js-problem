5bb923fca187ca3cf63e5f93614620a123b1de94
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-01-31 23:52:24 +0600

    floor

--- js-problem.js
+++ js-problem.js
@@ -3,6 +3,10 @@ const outPut = Math.abs(myNumber);
 console.log(outPut);
 
 
-const myNumber = 7.0322
+const myNumber = 7.0322;
 const outPut = Math.ceil(myNumber);
+console.log(outPut);
+
+const myNumber = 6.003;
+const outPut = Math.floor(myNumber);
 console.log(outPut);
