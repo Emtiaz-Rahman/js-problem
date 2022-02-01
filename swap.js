a2271507d5ab4b06b99bbb1288e0b3913dbae763
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-02-01 10:55:47 +0600

    distructring

--- swap.js
+++ swap.js
@@ -5,3 +5,11 @@ var temp = first;
 first = second;
 second = temp;
 console.log(first, second);
+
+// destructring
+var first = 5;
+var second = 7;
+console.log(first, second);
+[first, second] = [second, first];
+console.log(first, second);
+
