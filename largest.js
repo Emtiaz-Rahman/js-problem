78a2cf92e325edf2d33c0d1212cf23576016b5b7
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-02-01 20:12:20 +0600

    largest1

--- largest.js
+++ largest.js
@@ -12,3 +12,18 @@ function largestElement(numbers) {
 const ages = [12, 22, 45, 70, 11];
 const oldest = largestElement(ages);
 console.log('Oldest Man=', oldest);
+
+
+function largestMan(ages) {
+    let largest = 0;
+    for (let i = 0; i < ages.length; i++) {
+        const element = ages[i];
+        if (element > largest) {
+            largest = element;
+        }
+    }
+    return largest;
+}
+const ages = [12, 22, 32, 52, 80];
+const oldest = largestMan(ages);
+console.log('Oldest Man=', oldest);
