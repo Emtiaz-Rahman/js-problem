17d3ba44a2015b952c96738c7821e5334e7329bd
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-02-01 10:49:50 +0600

    swap

--- js-problem.js
+++ js-problem.js
@@ -1,16 +1,27 @@
-// const myNumber = -5;
-// const outPut = Math.abs(myNumber);
-// console.log(outPut);
+const myNumber = -5;
+const outPut = Math.abs(myNumber);
+console.log(outPut);
 
 
-// const myNumber = 7.0322;
-// const outPut = Math.ceil(myNumber);
-// console.log(outPut);
+const myNumber = 7.0322;
+const outPut = Math.ceil(myNumber);
+console.log(outPut);
 
-// const myNumber = 6.003;
-// const outPut = Math.floor(myNumber);
-// console.log(outPut);
+const myNumber = 6.003;
+const outPut = Math.floor(myNumber);
+console.log(outPut);
 
 const myNumber = 6.403;
 const outPut = Math.round(myNumber);
 console.log(outPut);
+
+for (let i = 0; i <= 30; i++) {
+    const outPut = Math.random() * 6;
+    const rounded = Math.round(outPut);
+    console.log(rounded);
+}
+
+
+
+
+
